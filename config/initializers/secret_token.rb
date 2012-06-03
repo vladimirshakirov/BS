b95181a2a9843474840f848d9876b55f217794b1
--global <--global>
2012-06-03 20:21:07 +0400

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BesimpleApp::Application.config.secret_token = 'cde90cdfd6ef75119fd1f420787dc79fb952a3cacc96b08afcc7f9e1f4b46498e1c2474ef57f271cea7bff1415fe58c826d40f3b12180d385d8e5a56b93c6b61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BesimpleApp::Application.config.session_store :cookie_store, key: '_besimple_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BesimpleApp::Application.config.session_store :active_record_store
